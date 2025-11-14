eceb64575d8d26c870af049a59e0533a60bbac9e
jessilin007@gmail.com <jessilin007@gmail.com>
2025-11-14 14:46:59 +0100

    save

--- scripts/birds.js
+++ scripts/birds.js
@@ -1,5 +1,5 @@
 
-//birds//
+// birds//
 
 function Bird (name,lifespan,group,food,length,weight,found,image){
     this.name = name ,
@@ -9,63 +9,69 @@ function Bird (name,lifespan,group,food,length,weight,found,image){
     this.length =length,
     this.weight = weight,
     this.found = found,
-    this.image = image,
     this.detail = () => `${name} ${lifespan} ${group} ${food} ${length} ${weight} ${found} ${image} `
 }
 
-let cassowary = new Bird ("Cassowary",
-    "20 years",
+
+let birdsArray = [
+    new Bird
+    ("Cassowary","20 years",
     "birds",
     "Plants matter like fruit, insects and small animals like mice amd lizards",
     "1.7m",
     "44kg",
     "Queensland",
-    "./images/birds/cassowary.jpg")
+    ),
 
-let Kookaburra = new Bird("kookaburra","20 years",
+    new Bird(
+    "kookaburra","20 years",
     "birds",
-    "Plants matter like fruit, insects and small animals like mice amd lizards",
-    "1.7m",
-    "44kg",
-    "Queensland",
-    "./images/birds/kookaburra.jpg")
+    " Insects and small animals including snakes, frogs and lizards",
+    "43cm",
+    "300g",
+    "Australia wide",
+    "./images/birds/kookaburra.jpg"),
 
-let YellowTailedBlackCockatoo = new Bird ("YellowTailedBlackCockatoo","birds",
-    "Plants matter like fruit, insects and small animals like mice amd lizards",
-    "1.7m",
-    "44kg",
-    "Queensland",
-    "./images/birds/yellow-tailed-black-cockatoo.jpg")
+    new Bird 
+    ("YellowTailedBlackCockatoo","41 years",
+    "birds",
+    "Fruit, seeds and other plant material",
+    "65cm",
+    "900 grams",
+    "SE Australia",
+    )]
 
-let birds = [cassowary,Kookaburra,YellowTailedBlackCockatoo];
 
-birds.forEach(bird => {
+birdsArray.forEach(bird => {
     console.log(bird.detail());
 });
 
 
 
 
-
-//read more
-
-let detailList = document.querySelector(".detail__list");
-let detailImage = document.querySelector("img");
+// let birds = [cassowary,Kookaburra,YellowTailedBlackCockatoo];
 
 
-let gridSection = document.querySelector("#birdsContainer");
-let detailDescription = document.querySelector("#birdsDetail")
 
-birdsDetail.style.display = "none";
+// // read more
+
+// let detailList = document.querySelector(".detail__list");
+// let detailImage = document.querySelector("img");
 
 
-detailList.textContent = cassowary.detail();
-detailImage.src = cassowary.image;
-detailImage.alt = cassowary.name;
+// let gridSection = document.querySelector("#birdsContainer");
+// let detailDescription = document.querySelector("#birdsDetail")
 
-let readMoreBtn = document.querySelector(".bird__card .btns")
+// birdsDetail.style.display = "none";
 
-readMoreBtn.addEventListener("click", function () {
-    birdsDetail.style.display ="none";
-    birdsDetail.style.display ="block"
-});
+
+// detailList.textContent = cassowary.detail();
+// detailImage.src = cassowary.image;
+// detailImage.alt = cassowary.name;
+
+// let readMoreBtn = document.querySelector(".bird__card .btns")
+
+// readMoreBtn.addEventListener("click", function () {
+//     birdsDetail.style.display ="none";
+//     birdsDetail.style.display ="block"
+// });
