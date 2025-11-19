04f91d8936b84b50992972d97c65851c4ddf333d
jessilin007@gmail.com <jessilin007@gmail.com>
2025-11-19 10:59:24 +0100

    delete birdsgrop

--- scripts/birds.js
+++ scripts/birds.js
@@ -1,6 +1,5 @@
 const sidebar = document.querySelector(".sidebar");
 const content = document.querySelector(".content_container");
-const innerHTML = content.innerHTML; 
 
 
 function Birds(name, lifespan, group, food, length,  weight, found, image,description) {
@@ -22,22 +21,22 @@ let birdsArray = [
 
       "It’s not hard to imagine that cassowaries are descended from dinosaur ancestors. The largest cassowaries can stand as high as six feet and weigh up to 160 pounds.   These large birds cannot fly, but their extremely powerful legs propel them at great speeds. They are strong swimmers and can move quickly on both land and water. Cassowaries are shy and they are usually hard to spot, at least in their natural rain forest habitats. They are not overly aggressive, and attacks are rare. But they can do a lot of damage if they are provoked or angered. Cassowary attacks have occasionally been deadly, including a recent one which occurred in 2019, at a private collection of caged birds in Florida",),
 
-    new Birds("Kookaburra", "20 years", "Bird", "Insects and small animals including snakes, frogs and lizards",  "43cm", "300g", "Australia wide", "./images/birds/Kookaburra.jpg",
+    new Birds(
+      "Kookaburra", "20 years", "Bird", "Insects and small animals including snakes, frogs and lizards",  "43cm", "300g", "Australia wide", "./images/birds/Kookaburra.jpg",
       "Native to the eucalyptus forests of eastern Australia, the laughing kookaburra is the largest member of the Kingfisher family, with females weighing up to one pound and growing to 43 cm in length. Its beak can reach 10 cm long and is used to snatch a variety of invertebrates and small vertebrates, including the occasional small snake. Since being introduced in western Australia and New Zealand, the kookaburra has angered farmers by preying on their fowl. The laughing kookaburra has dark brown wing plumage and a white head and underside. Dark brown eye stripes run across its face and its upper bill is black. Its reddish-coloured tail is patterned with black bars."
     ),
 
     new Birds(
       "Yellow Tailed Cockatoo", "41 years", "Bird", "Fruit, seeds and other plant material", "65cm", "900 grams", "SE Australia", "./images/birds/yellow-tailed-black-cockatoo.jpg",
-    "It has a short crest on the top of its head. Its plumage is mostly brownish black and it has prominent yellow cheek patches and a yellow tail band. The body feathers are edged with yellow giving a scalloped appearance. The adult male has a black beak and pinkish-red eye-rings, and the female has a bone-coloured beak and grey eye-rings. In flight, yellow-tailed black cockatoos flap deeply and slowly, with a peculiar heavy fluid motion. Their loud, wailing calls carry for long distances. The whiteae is found south of Victoria to the East of South Australia and is smaller in size. The yellow-tailed black cockatoo is found in temperate forests and forested areas across south and central eastern Queensland to southeastern South Australia, including a very small population persisting in the Eyre Peninsula.[4] Two subspecies are recognised, although Tasmanian and southern mainland populations of the southern subspecies xanthanotus may be distinct enough from each other to bring the total to three. Birds of subspecies funereus (Queensland to eastern Victoria) have longer wings and tails and darker plumage overall, while those of xanthanotus (western Victoria, South Australia and Tasmania) have more prominent scalloping.")
+      "It has a short crest on the top of its head. Its plumage is mostly brownish black and it has prominent yellow cheek patches and a yellow tail band. The body feathers are edged with yellow giving a scalloped appearance. The adult male has a black beak and pinkish-red eye-rings, and the female has a bone-coloured beak and grey eye-rings. In flight, yellow-tailed black cockatoos flap deeply and slowly, with a peculiar heavy fluid motion. Their loud, wailing calls carry for long distances. The whiteae is found south of Victoria to the East of South Australia and is smaller in size. The yellow-tailed black cockatoo is found in temperate forests and forested areas across south and central eastern Queensland to southeastern South Australia, including a very small population persisting in the Eyre Peninsula.[4] Two subspecies are recognised, although Tasmanian and southern mainland populations of the southern subspecies xanthanotus may be distinct enough from each other to bring the total to three. Birds of subspecies funereus (Queensland to eastern Victoria) have longer wings and tails and darker plumage overall, while those of xanthanotus (western Victoria, South Australia and Tasmania) have more prominent scalloping.")
 ];
 
 const maxLength = desc => desc.slice(0, 200) + (desc.length > 200 ? "..." : "");
 
 
-//sidebar
+sidebar
 let birdGroup = document.createElement("birdGroup");
     birdGroup.classList.add("animal_group")
-    birdGroup.textContent ="Birds:"
 
     sidebar.appendChild(birdGroup)
 
@@ -47,13 +46,13 @@ let birdGroup = document.createElement("birdGroup");
 
 birdsArray.forEach(birds => {
     let item = document.createElement("div");
-        item.classList.add("animal_item");
+        item.classList.add("sidebar_item");
         item.textContent = birds.name;
 
         item.addEventListener("click", (e) => {
           e.stopPropagation();
 
-        document.querySelectorAll(".animal_item").forEach(i => i.classList.remove("active_item"));
+        document.querySelectorAll(".sidebar_item").forEach(i => i.classList.remove("active_item"));
         item.classList.add("active_item");
         
         showShortSummary(birds);
